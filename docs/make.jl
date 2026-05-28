24504292b649082a4b94fd52915cbb5320060a12
Boris Blagov <46528899+borisblagov@users.noreply.github.com>
2026-05-28 12:58:32 +0200

    fixing missing comas in pages in makedocs() in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,15 +7,15 @@ makedocs(
     pages = [
         "Introduction" => "introduction.md",
         "Estimation" => [
-            "Chan2020minn" => "Chan2020minn.md"
-            "Chan2020iniw" => "Chan2020iniw.md"
+            "Chan2020minn" => "Chan2020minn.md",
+            "Chan2020iniw" => "Chan2020iniw.md",
         ],
         "Forecasting" => "forecasting.md",
         "Structural analysis" => "irfs.md",
         "File library" => [
-            "Constructors" => "Constructors.md"
-            "Initialization" => "init_functions.md"
-            "Data transformation" => "dataPrep.md"
+            "Constructors" => "Constructors.md",
+            "Initialization" => "init_functions.md",
+            "Data transformation" => "dataPrep.md",
         ]
     ]
 )
