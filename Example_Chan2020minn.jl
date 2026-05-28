6062844f681cc85ea3c03a1655d08f45dbb270b7
Boris Blagov <46528899+borisblagov@users.noreply.github.com>
2026-05-28 14:26:19 +0200

    renaming strct to struct in Example_Chan2020min.jl [skip ci]

--- Example_Chan2020minn.jl
+++ Example_Chan2020minn.jl
@@ -2,10 +2,10 @@ using BEAVARs, TimeSeries
 using Dates     # these are required only for the example, your data may already have a time-series format
 
 # Chan2020minn
-model_type, set_strct, hyp_strct= makeSetup("Chan2020minn",n_burn=20;n_save=50,p=2)
+model_type, set_struct, hyp_struct= makeSetup("Chan2020minn",n_burn=20;n_save=50,p=2)
 data = TimeArray(DateTime(2020,1,1):Quarter(1):DateTime(2027,4,1),rand(30,3))
-data_strct = makeDataSetup(model_type,data)
-out_strct = beavar(model_type, set_strct, hyp_strct, data_strct)
+data_struct = makeDataSetup(model_type,data)
+out_struct = beavar(model_type, set_struct, hyp_struct, data_struct)
 
-fcast_strct = BEAVARs.forecast(out_strct, set_strct, data_strct)
-BEAVARs.forecast_plot(fcast_strct)
+fcast_struct = BEAVARs.forecast(out_struct, set_struct, data_struct)
+BEAVARs.forecast_plot(fcast_struct)
